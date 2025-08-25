7a8c7d8b023bb45af5f3c22ce9e27163bea66fc4
Sander Voogt <sandervoogt@outlook.com>
2025-08-25 15:45:45 +0200

    update

--- backend/src/api/admin/carbrands/validators.ts
+++ backend/src/api/admin/carbrands/validators.ts
@@ -1,2 +1,9 @@
 import { z } from "zod"
 
+export const PostAdminCreateCarBrand = z.object({
+    name: z.string(),
+    title: z.string().optional(),
+    intro: z.string().optional(),
+    description: z.string().optional(),
+    image: z.string().optional()
+})
